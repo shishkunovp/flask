e96a85411dc228a2259839b3846e9bb3d46697d5
shishkunovp <147750399+shishkunovp@users.noreply.github.com>
2025-01-05 20:44:32 +0300

    Add db

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+db: SQLAlchemy = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -35,6 +38,25 @@ def signup():
         # Здесь можно добавить логику проверки/сохранения
         return f"Регистрация успешна. Пользователь: {username}"
     return render_template('signup.html')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+@app.route('/add_users', methods=['GET', 'POST'])
+def add_users():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return f"Пользователь: {username} добавлен"
+    return render_template('add_users.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
