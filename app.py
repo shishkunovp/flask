6334ed29b11595c5481680ebe2d231d5a3e5d918
shishkunovp <147750399+shishkunovp@users.noreply.github.com>
2025-01-05 12:44:24 +0300

    Add route and POST 2

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask import request
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Привет, Flask!"
+
+@app.route('/hello/<name>')
+def say_hello(name):
+    return f"Hello, {name}!"
+
+
+@app.route('/calc', methods=['POST'])
+def calc_sum():
+    data = request.get_json()
+    a = data['a']
+    b = data['b']
+    return {'result': a + b}
 
 if __name__ == '__main__':
     app.run(debug=True)
